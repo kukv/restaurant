5eb5d2f18693069e9998f5a33d2c6401f5b225d4
kukv <rio.by.kukv@gmail.com>
2024-03-02 18:41:35 +0900

    予約リポジトリの具体的な処理を追加

---
+++ internal-api/reservations/src/main/java/jp/kukv/reservations/infrastructure/datasource/reservation/ReservationRecordMapper.java
@@ -0,0 +1,15 @@
+package jp.kukv.reservations.infrastructure.datasource.reservation;
+
+import jp.kukv.reservations.domain.model.reservation.Reservation;
+import jp.kukv.reservations.domain.model.reservation.ReservationId;
+import jp.kukv.reservations.infrastructure.datasource.reservation.course.CreatedTime;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+interface ReservationRecordMapper {
+  void record(
+      @Param("reservationId") ReservationId reservationId,
+      @Param("reservation") Reservation reservation,
+      @Param("createdTime") CreatedTime createdTime);
+}
