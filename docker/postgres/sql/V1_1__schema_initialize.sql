5251b604aac5c5fd43bb04f74c85605bfc687461
kukv <rio.by.kukv@gmail.com>
2024-03-02 12:46:38 +0900

    予約情報を管理するスキーマを追加

--- docker/postgres/sql/V1_1__schema_initialize.sql
+++ docker/postgres/sql/V1_1__schema_initialize.sql
@@ -1,3 +1,7 @@
 drop schema if exists sandbox cascade;
 create schema sandbox;
 comment on schema sandbox is 'sandboxスキーマ';
+
+drop schema if exists reservations cascade;
+create schema reservations;
+comment on schema reservations is '予約情報を管理するスキーマ';
