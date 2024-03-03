8428e527230189953a2cbfc3859a8fb5a3324bc5
kukv <rio.by.kukv@gmail.com>
2024-03-03 17:19:26 +0900

    単体テストを追加

--- internal-api/reservations/src/main/java/jp/kukv/reservations/domain/model/identify/DinerId.java
+++ internal-api/reservations/src/main/java/jp/kukv/reservations/domain/model/identify/DinerId.java
@@ -1,12 +1,16 @@
 package jp.kukv.reservations.domain.model.identify;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 /** ディナー客ID */
 public class DinerId {
 
   @NotNull(message = "ディナー客IDは必須")
+  @Min(value = 1, message = "ディナー客IDは1以上")
+  @Max(value = 2_000_000_000, message = "ディナー客IDは2,000,000,000以下")
   @JsonValue
   Integer value;
 
